250fec61fdfa39ba52c185ec5f018780e892a9ba
deepalisrivastava24 <deepali.srivastava24@gmail.com>
2026-02-10 16:52:30 +0530

    Vehicle Inheritance Hierarchy

---
+++ src/Test/Vehicle_Inheritance_Hierarchy.java
@@ -0,0 +1,30 @@
+package Test;
+
+public class Vehicle_Inheritance_Hierarchy {
+    public static void main(String[] args) {
+        //Vehicle[] vehicles = {new Car(), new Vehicle()};
+        Vehicle[] vehicles = {new Car(), new Bike() };
+
+        for (Vehicle vehicle : vehicles) {
+            vehicle.start();
+        }
+    }
+}
+
+class Vehicle{
+    void start(){
+        System.out.println("Vehicle starting");
+    }
+}
+
+class Car extends Vehicle{
+    void start(){
+        System.out.println("Car engine starts with a roar!");
+    }
+}
+
+class Bike extends Vehicle{
+    void start(){
+        System.out.println("Bike engine starts with a purr!");
+    }
+}
