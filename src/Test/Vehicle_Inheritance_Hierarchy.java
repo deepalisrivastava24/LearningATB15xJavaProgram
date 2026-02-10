8ef62de07b793706a4436b90c2d470f978c8ac71
deepalisrivastava24 <deepali.srivastava24@gmail.com>
2026-02-10 16:54:29 +0530

    Vehicle Inheritance Hierarchy

--- src/Test/Vehicle_Inheritance_Hierarchy.java
+++ src/Test/Vehicle_Inheritance_Hierarchy.java
@@ -18,12 +18,14 @@ class Vehicle{
 }
 
 class Car extends Vehicle{
+    @Override
     void start(){
         System.out.println("Car engine starts with a roar!");
     }
 }
 
 class Bike extends Vehicle{
+    @Override
     void start(){
         System.out.println("Bike engine starts with a purr!");
     }
